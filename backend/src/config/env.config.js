e17d9c722d3e01b31935dcf648e7189f2da08609
bumi-il <mg0552974085@gmail.com>
2026-05-09 12:34:05 +0300

    Added jwt to env

--- backend/src/config/env.config.js
+++ backend/src/config/env.config.js
@@ -4,6 +4,7 @@ config();
 export const env = {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
     NODE_ENV: process.env.NODE_ENV,
 };
